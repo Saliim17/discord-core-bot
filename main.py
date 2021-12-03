b0f1182b22077ad7d49fbac777e49d66f97cd523
Saliim17 <ahouchsalim@gmail.con>
2021-12-03 19:34:09 +0100

    insert new messages

--- main.py
+++ main.py
@@ -1,30 +1,38 @@
 import discord
 import os
+from discord import reaction
+from discord.message import Message
 from dotenv import load_dotenv
 load_dotenv()
 import random
+from discord.ext    import commands
+from discord.ext.commands   import Bot
+import asyncio
 
 client = discord.Client()
 
 algorithm = ["algoritmica"]
 ac = ["ac", "arquitectura", "arquitectura de computadores"]
 baco_ailajas = ["paco", "aylagas", "paco aylagas", "Paco", "Aylagas", "Paco Aylagas", "PACO", "AYLAGAS", "PACO AYLAGAS"]
-suspender = ["suspender", "suspenso"]
+suspender = ["suspender", "suspenso", "suspendo", "suspendería", "he suspendido", "suspendió"]
 mejor = ["mejor", "makina", "bueno"]
 starter_algorithms = [
-    "Nah, algorítimica está chupado. Yo saqué un 9.:tired_face:",
-    "Algorítmica es imposible de aprobar.:tired_face:",
-    "Backtracking:tired_face:."
+    "Nah, algorítimica está chupado. Yo saqué un 9. 😁",
+    "Algorítmica es imposible de aprobar. 😢",
+    "Backtracking 🤪"
 ]
 starter_ac = [
-    "Nah, AC es fácil lo que pasa es que no estudias.",
-    "Yo no se ni lo que es un fetch.:tired_face:",
-    "Forwarding.:tired_face:"
+    "Nah, AC es fácil lo que pasa es que no estudias.😈",
+    "Yo no se ni lo que es un fetch. 😴",
+    "Forwarding 🥴"
 ]
 starter_paco = [
-    "Paco Aylagas es el mejor profesor de la ETSISI.",
-    "Quién no conoce a Paco a cualquier Miñano le reza.:palms_up_together:"
+    "Paco Aylagas es el mejor profesor de la ETSISI. ❤️‍🔥",
+    "Quién no conoce a Paco a cualquier Miñano le reza. :palms_up_together:"
 ]
+
+ayuda = ["help", "ayuda"]
+bot = ["bot", "Coru", "coru"]
 
 @client.event
 async def on_ready():
@@ -37,9 +45,12 @@ async def on_message(message):
         return
 
     msg = message.content
-    if msg.startswith('$hello'):
-        await message.channel.send('Hello!')
+    #if msg.startswith(''):
+     #   await message.channel.send('Hello!')
 
+    if any(word in msg for word in ayuda) or any(word in msg for word in bot):
+        msg1 = await message.channel.send("Select language:")
+        await client.add_reaction(msg1, ":flag_es:")
     if any(word in msg for word in algorithm) and any(word in msg for word in suspender):
         await message.channel.send(random.choice(starter_algorithms))
 
@@ -48,7 +59,6 @@ async def on_message(message):
 
     if any(word in msg for word in baco_ailajas) and any(word in msg for word in mejor):
         await message.channel.send(random.choice(starter_paco))
-
 
 
 client.run(os.getenv("BOT_TOKEN"))
